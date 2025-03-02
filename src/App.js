b35e019b093e1d576b88685dfda7bb8b868e67cc
Deny <denylson.feitoza@alunos.ufersa.edu.br>
2025-03-02 16:00:40 -0300

    Remover Vercel Analytics do componente App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { GlobalStyles } from "./styles/globalStyles";
 import HomePage from "./pages/Home";
 import CameraPage from "./pages/CameraPage";
 import ResultPage from "./pages/ResultPage";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
@@ -18,7 +17,6 @@ function App() {
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <GlobalStyles />
-      <Analytics />
       <Routes>
         <Route
           path="/"
